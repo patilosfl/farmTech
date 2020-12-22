af6475c96f9a64f5cd8df745e2757fbd3fbcc4fa
PRAVINSURFACE\pravin <shrirangosfl@gmail.com>
2020-12-22 16:07:34 +0530

    no changes

---
+++ src/app/services/customer.ts
@@ -0,0 +1,17 @@
+export interface Customer {
+    id?:string;
+    name?: string;
+    address?:string;
+    land?:string;
+    relation?:string;
+    documents?:string;
+    image?:string;
+    dob?:string;
+    contact?:string;
+    zipcode?:string;
+    location?:string;
+    district?:string
+}
+
+
+//
